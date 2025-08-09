866aa99073bee7f698690513e4318edc749208d3
Jesvir Zuniega <jeszuniega@gmail.com>
2025-08-09 10:57:47 +0800

    add line

--- src/app/PdfViewer.tsx
+++ src/app/PdfViewer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { btn, Mode } from "./page";
 import * as pdfjsLib from "pdfjs-dist";
 pdfjsLib.GlobalWorkerOptions.workerSrc = "./pdf.worker.mjs";
@@ -10,11 +10,21 @@ interface Props {
   setTool: (tool: Mode | null) => void;
 }
 
+interface Point {
+  x: number;
+  y: number;
+}
+
+type Line = [Point, Point];
+
 export default function PdfViewer({ tool, setTool }: Props) {
   const [fileBuffer, setFileBuffer] = useState<Uint8Array|null>(null);
   const pdfCanvasRef = useRef<HTMLCanvasElement>(null);
   const pdfCanvasOverlayRef = useRef<HTMLDivElement>(null);
+  const pdfDrawCanvasRef = useRef<HTMLCanvasElement>(null);
   const [pageSize, setPageSize] = useState({ width: 0, height: 0 });
+  const [renderedLines, setRenderedLines] = useState<Line[]>([]);
+  const [lines, setLines] = useState<Point[]>([]);
 
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const f = e.target.files?.[0];
@@ -33,16 +43,20 @@ export default function PdfViewer({ tool, setTool }: Props) {
 
   async function renderPdfPage(page: pdfjsLib.PDFPageProxy, scale: number) {
     const viewport = page.getViewport({ scale: scale, });
-    // Support HiDPI-screens.
     const outputScale = window.devicePixelRatio || 1;
 
     const canvas = pdfCanvasRef.current!;
+    const canvasDraw = pdfDrawCanvasRef.current!;
     const context = canvas.getContext('2d');
 
     canvas.width = Math.floor(viewport.width * outputScale);
     canvas.height = Math.floor(viewport.height * outputScale);
     canvas.style.width = Math.floor(viewport.width) + "px";
     canvas.style.height =  Math.floor(viewport.height) + "px";
+    canvasDraw.width = Math.floor(viewport.width * outputScale);
+    canvasDraw.height = Math.floor(viewport.height * outputScale);
+    canvasDraw.style.width = Math.floor(viewport.width) + "px";
+    canvasDraw.style.height =  Math.floor(viewport.height) + "px";
 
     const transform = outputScale !== 1
       ? [outputScale, 0, 0, outputScale, 0, 0]
@@ -62,11 +76,12 @@ export default function PdfViewer({ tool, setTool }: Props) {
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
     if (tool === 'text') spawnTextBox(x, y);
+    if (tool === 'line') spawnLine(x, y);
   };
 
   const spawnTextBox = (x: number, y: number) => {
     const textBox = document.createElement('textarea');
-    textBox.className = 'absolute max-w-[240px] h-auto text-black text-2xl p-1 bg-transparent focus:outline-none border-none z-10 resize-none overflow-hidden';
+    textBox.className = 'absolute max-w-[240px] h-auto text-sm p-5 border-1 border-dashed border-transparent focus:border-black text-black text-2xl bg-transparent focus:outline-none z-10 resize-none overflow-hidden';
     textBox.style.left = `${x}px`;
     textBox.style.top = `${y}px`;
     textBox.value = '';
@@ -80,6 +95,55 @@ export default function PdfViewer({ tool, setTool }: Props) {
     setTool(null);
   };
 
+  const spawnLine = (x: number, y: number) => {
+    setLines([...lines, { x, y }]);
+  };
+
+  const drawLine = (context: CanvasRenderingContext2D, { x: x1, y: y1 }: Point, { x: x2, y: y2 }: Point) => {
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+  }
+
+  useEffect(() => {
+    const canvas = pdfDrawCanvasRef.current!;
+    const context = canvas.getContext('2d')!;
+
+    const redraw = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      renderedLines.forEach(([p1, p2]) => drawLine(context, p1, p2));
+    }
+
+    const previewLineOnMouseMove = (e: MouseEvent) => {
+      redraw();
+      drawLine(context, lines[0], { x: e.offsetX, y: e.offsetY });
+    }
+
+    const removePreviewLineOnMouseUp = () => {
+      document.removeEventListener('mousemove', previewLineOnMouseMove);
+    }
+
+    if (lines.length === 1) {
+      document.addEventListener('mousemove', previewLineOnMouseMove);
+      document.addEventListener('mouseup', removePreviewLineOnMouseUp);
+    }
+
+    console.log(lines);
+    if (lines.length === 2) {
+      removePreviewLineOnMouseUp();
+      redraw();
+      drawLine(context, lines[0], lines[1]);
+      setLines([]);
+      setRenderedLines([...renderedLines, [lines[0], lines[1]]]);
+    }
+
+    return () => {
+      document.removeEventListener('mousemove', previewLineOnMouseMove);
+      document.removeEventListener('mouseup', removePreviewLineOnMouseUp);
+    }
+  }, [lines, renderedLines]);
+
 
   return <div className="flex flex-col items-center justify-center">
     <input type="file" id="pdf-file" className="hidden" accept="application/pdf" onChange={handleFileChange}/>
@@ -87,8 +151,9 @@ export default function PdfViewer({ tool, setTool }: Props) {
       Upload PDF
     </label>
     <div className="relative overflow-hidden">
-      <canvas id="canvas" ref={pdfCanvasRef} className={`bg-white rounded-2xl shadow-2xl relative`}></canvas>
-      <div id="canvas-overlay" ref={pdfCanvasOverlayRef} className={`absolute overflow-hidden top-0 left-0 w-full h-full`} onClick={onClick}></div>
+      <canvas id="canvas" ref={pdfCanvasRef} className={`rounded-2xl shadow-2xl relative w-full h-full`}></canvas>
+      <canvas id="canvas-draw" ref={pdfDrawCanvasRef} className={`absolute overflow-hidden top-0 left-0 w-full h-full z-10`}></canvas>
+      <div id="canvas-overlay" ref={pdfCanvasOverlayRef} className={`absolute overflow-hidden top-0 left-0 w-full h-full z-20`} onClick={onClick}></div>
     </div>
   </div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,25 @@ export const btnActive = 'bg-[#da3668]'
 const PdfViewer = dynamic(() => import('./PdfViewer'), { ssr: false });
 
 export default function Home() {
-  const [tool, setTool] = useState<Mode|null>("text");
+  const [tool, setTool] = useState<Mode|null>(null);
 
   return (
     <div className="min-w-screen min-h-screen bg-gradient-to-t from-black to-[#731c37]">
-      <header className="fixed top-5 flex w-full justify-center">
+      <header className="fixed top-5 flex w-full justify-center z-20">
         <div className="flex text-lg bg-[#1c1618] shadow-xl p-1 rounded-2xl gap-2">
+          <button type="button" className={`${btn} ${tool === null ? btnActive : ''}`} onClick={() => setTool(null)}>
+            Mouse
+          </button>
           <button type="button" className={`${btn} ${tool === 'text' ? btnActive : ''}`} onClick={() => setTool('text')}>
             Text
           </button>
           <button type="button" className={`${btn} ${tool === 'line' ? btnActive : ''}`} onClick={() => setTool('line')}>
             Line
           </button>
-          <button type="button" className={`${btn}`} onClick={() => setTool(null)}>
-            x
-          </button>
         </div>
       </header>
       <main className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center mt-[10vh] mb-10">
+        <div className="flex flex-col items-center justify-center mt-[20vh] mb-10">
           <h1 className="text-6xl mb-5">PDF Polygon Annotator</h1>
           <p className="mb-5">Annotate PDFs with polygons and text</p>
         </div>
